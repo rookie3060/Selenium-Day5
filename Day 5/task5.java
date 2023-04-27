443b219edfcd97d50c41cf24bc7c8186f0b0b887
rookie3060 <98405422+rookie3060@users.noreply.github.com>
2023-04-27 09:28:18 +0530

    Add files via upload

--- Day 5/task5.java
+++ Day 5/task5.java
@@ -1,19 +1,45 @@
 package day_5;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class task5 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WebDriverManager.chromedriver().setup();
 		System.setProperty("webdriver.chrome.driver","c://Drivers//chromedriver.exe" );
-		ChromeOptions co = new ChromeOptions();
-		co.addArguments("--remote-allow-origins=*");
-		WebDriver driver = new ChromeDriver(co);
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origin=*");
+		WebDriver driver=new ChromeDriver(co);
 		driver.get("https://www.google.com");
-		
-
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("Apple");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		System.out.print("\n"+driver.getWindowHandle());
+		System.out.print("\n"+driver.getTitle());
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		System.out.print("\n"+driver.getWindowHandle());
+		System.out.print("\n"+driver.getTitle());
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("cucumber");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		System.out.print("\n"+driver.getWindowHandle());
+		System.out.print("\n"+driver.getTitle());
+		Set<String>s=driver.getWindowHandles();
+		System.out.println("Number of windows opened="+s.size());
 	}
-
 }
